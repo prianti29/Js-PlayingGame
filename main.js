de6fbf4577d617d485d5462362128fdd702341eb
prianti29 <59369461+prianti29@users.noreply.github.com>
2023-03-06 17:19:57 +0600

    initial

---
+++ main.js
@@ -0,0 +1,13 @@
+const player1ScoreDisplay = document.getElementById('player1Score');
+const player2ScoreDisplay = document.getElementById('player2Score');
+
+const winingScoreDisplay = document.querySelector('p span');
+
+const inputScore = document.getElementById('inputScore');
+const player1Btn = document.getElementById('player1Btn');
+const player2Btn = document.getElementById('player2Btn');
+const resetBtn = document.getElementById('resetBtn');
+
+const player1Score = 0;
+const player2Score = 0;
+const winingScore = 5;
