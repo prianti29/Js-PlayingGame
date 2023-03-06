77e7332876b79d2f4b502e233d91e77cab4bcec3
prianti29 <59369461+prianti29@users.noreply.github.com>
2023-03-06 21:09:59 +0600

    player 1 & 2 data changes

--- main.js
+++ main.js
@@ -7,6 +7,33 @@ const inputScore = document.getElementById('inputScore');
 const player1Btn = document.getElementById('player1Btn');
 const player2Btn = document.getElementById('player2Btn');
 const resetBtn = document.getElementById('resetBtn');
-const player1Score = 0;
-const player2Score = 0;
+let player1Score = 0;
+let player2Score = 0;
 const winingScore = 5;
+let gameOver = false;
+
+player1Btn.addEventListener('click', () => {
+    // console.log('clicked Player 1');
+    // 1. Change Data
+   if(!gameOver){
+    player1Score ++;
+    if(player1Score === winingScore){
+        gameOver = true;
+        player1Btn.setAttribute('disabled', 'disabled');
+        player2Btn.setAttribute('disabled', 'disabled');
+    }
+   }
+    // 2.Show changed data working with dom
+    player1ScoreDisplay.textContent = player1Score;
+});
+player2Btn.addEventListener('click', () => {
+    if(!gameOver){
+        player2Score ++;
+    if(player2Score === winingScore){
+        gameOver = true;
+        player1Btn.setAttribute('disabled', 'disabled');
+        player2Btn.setAttribute('disabled', 'disabled');
+    }
+    }
+    player2ScoreDisplay.textContent = player2Score;
+});
